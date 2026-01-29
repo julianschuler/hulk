c5042b93f933b1f782aa07cfb34ee74702509ce2
Julian Schuler <julianschuler@users.noreply.github.com>
2026-01-29 19:14:32 +0100

    Forward high level interface messages

--- crates/zenoh_bridge/src/main.rs
+++ crates/zenoh_bridge/src/main.rs
@@ -7,6 +7,7 @@ use std::fmt::Debug;
 use booster::{ButtonEventMsg, FallDownState, LowCommand, LowState};
 use color_eyre::eyre::{Result, WrapErr};
 use futures_util::{future::Fuse, select, FutureExt};
+use high_level_interface::Request;
 use ros2::sensor_msgs::{camera_info::CameraInfo, image::Image};
 use ros2_client::{
     ros2::{
@@ -171,6 +172,14 @@ async fn main() -> Result<()> {
         MessageTypeName::new("booster_interface", "LowCmd"),
         "joint_ctrl",
     )?;
+    let mut loco_api_forwarder = spawn_zenoh_to_ros_forwarder::<Request>(
+        &mut ros_node,
+        zenoh_session.clone(),
+        "/",
+        "LocoApiTopicReq",
+        MessageTypeName::new("booster_msgs", "RpcReqMsg"),
+        "high_level_interface",
+    )?;
 
     // If no errors occur, none of these futures will complete
     let result = select! {
@@ -190,6 +199,7 @@ async fn main() -> Result<()> {
         result = image_right_raw_forwarder => result,
         result = image_right_raw_camera_info_forwarder => result,
         result = low_command_forwarder => result,
+        result = loco_api_forwarder => result,
     }
     .wrap_err("failed to run forwarder to completion")?;
 
